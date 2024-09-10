d3d810492435035bd9389fedf4034d75d573a8e6
Omar <omarkhaled1752003@yahoo.com>
2024-09-10 15:44:16 +0300

    url for TaskCreate view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import register_user, login_user, ViewLogs
+from .views import (
+    register_user,
+    login_user,
+    ViewLogs,
+    ListCreateTask,
+    RetrieveUpdateDeleteTask,
+)
 
 urlpatterns = [
     path("register/", register_user, name="register"),
     path("login/", login_user, name="login"),
     path("view-logs/", ViewLogs.as_view(), name="view-logs"),
+    path("tasks/", ListCreateTask.as_view(), name="list-create-tasks"),
 ]
