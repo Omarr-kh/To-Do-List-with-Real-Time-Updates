ba380e0e343dbdbbdf0abc68c8ae0bc4939c92f3
Omar <omarkhaled1752003@yahoo.com>
2024-09-10 17:41:38 +0300

    modified IsOwner custom permission

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -1,9 +1,20 @@
 from rest_framework import permissions
 
 
-class isOwner(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == "GET":
-            return True
+        if request.method == "DELETE":
+            return request.user == obj.owner
         else:
-            return request.user == obj.user
+            return request.user == obj.owner or obj.members.filter(user=request.user).exists()
+
+
+# class IsMember(permissions.BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+#             return True
+
+#         # Check if the request user is the owner of the task or a member
+#         return (
+#             request.user == obj.owner or obj.members.filter(user=request.user).exists()
+#         )
