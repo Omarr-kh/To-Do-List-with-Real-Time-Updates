de880b7571d53ac4631cfd00aebf20f9e7bc5dac
Omar <omarkhaled1752003@yahoo.com>
2024-09-10 15:44:28 +0300

    TaskCreate view

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.contrib.auth.models import User
 
-from .serializers import ActivityLogSerializer
+from .serializers import ActivityLogSerializer, TaskSerializer
 from .models import ActivityLog, Task, TaskMember
+from .permissions import isOwner
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import ValidationError
 
 from fcm_django.models import FCMDevice
 
@@ -36,14 +39,13 @@ def register_user(request):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        user = User.objects.create(
-            username=username, email=email, password=password
-        )
+        user = User.objects.create(username=username, email=email, password=password)
         Token.objects.create(user=user)
 
         return Response(
             {"message": "User registered successfully!"}, status=status.HTTP_201_CREATED
         )
+
 
 @api_view(["POST"])
 def login_user(request):
@@ -55,13 +57,15 @@ def login_user(request):
 
         if not username or not password or not registration_token or not device_type:
             return Response(
-                {"error": "Username, Password, registration_id, and type are required!"},
+                {
+                    "error": "Username, Password, registration_id, and type are required!"
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
         if not User.objects.filter(username=username, password=password).exists():
             return Response(
                 {"error": "Username or password are incorrect!"},
-                status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_400_BAD_REQUEST,
             )
         user = User.objects.get(username=username, password=password)
         token = Token.objects.get(user=user)
@@ -76,3 +80,30 @@ def login_user(request):
                 user=user, registration_id=registration_token, type=device_type
             )
         return Response({"token": {token.key}})
+
+
+class ListCreateTask(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Get tasks where the user is either the owner or a member
+        return Task.objects.filter(Q(owner=user) | Q(members__user=user)).distinct()
+
+    def perform_create(self, serializer):
+        task = serializer.save()
+        members = self.request.data.get("members", [])
+        if members:
+            for username in members:
+                try:
+                    user = User.objects.get(username=username)
+                    TaskMember.objects.create(task=task, user=user)
+                except User.DoesNotExist:
+                    raise ValidationError(f"user {username} doesn't exist")
+
+
+class RetrieveUpdateDeleteTask(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, isOwner]
