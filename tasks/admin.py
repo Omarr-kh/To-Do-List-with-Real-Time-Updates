c04efc0cce9c61b8bed8f828274fc34c01d7c89d
Omar <omarkhaled1752003@yahoo.com>
2024-09-10 14:56:46 +0300

    registered activity log model into admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Task, TaskMember
+from .models import Task, TaskMember, ActivityLog
 
 admin.site.register(Task)
 admin.site.register(TaskMember)
+admin.site.register(ActivityLog)
