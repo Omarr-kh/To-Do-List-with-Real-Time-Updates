b9ee3f4ac6818d738604a1a36f2cb5c54f03bc76
Omar <omarkhaled1752003@yahoo.com>
2024-09-11 16:56:12 +0300

    converted all class based views into function based views

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,19 +2,21 @@ from django.urls import path
 from .views import (
     register_user,
     login_user,
-    ViewLogs,
-    ListCreateTask,
-    RetrieveUpdateDeleteTask,
+    view_logs,
+    list_tasks,
+    view_task,
+    create_task,
+    update_task,
+    delete_task,
 )
 
 urlpatterns = [
     path("register/", register_user, name="register"),
     path("login/", login_user, name="login"),
-    path("view-logs/", ViewLogs, name="view-logs"),
-    path("tasks/", ListCreateTask.as_view(), name="list-create-tasks"),
-    path(
-        "tasks/<int:pk>/",
-        RetrieveUpdateDeleteTask.as_view(),
-        name="retrieve-update-delete-tasks",
-    ),
+    path("view-logs/", view_logs, name="view-logs"),
+    path("tasks/", list_tasks, name="list-tasks"),
+    path("create-task/", create_task, name="create-task"),
+    path("view-task/<int:pk>/", view_task, name="view-task"),
+    path("update-task/<int:pk>/", update_task, name="update-task"),
+    path("delete-task/<int:pk>/", delete_task, name="delete-task"),
 ]
